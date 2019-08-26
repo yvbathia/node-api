1e3b027b84e2c736d58c3847282c2ced7346ba05
Yash <yash.bathia@techholding.co>
2019-08-26 16:22:15 +0530

    follow up

--- api/product/product.js
+++ api/product/product.js
@@ -1,94 +1,87 @@
-const prodctModelContructor =  require('./product-model');
+const prodctModelContructor = require("./product-model");
 const productModel = new prodctModelContructor().model;
-const fs = require('fs');
+const fs = require("fs");
 
-class Product{
-
-    async getProducts(req,res){
-        try {
-            const product = await productModel.find({});
-            res.send(product);       
-        } catch (error) {
-            res.send({'Error':'Error getting products'});    
-        }
-     
+class Product {
+  async getProducts(req, res) {
+    try {
+      const product = await productModel.find({});
+      res.send(product);
+    } catch (error) {
+      res.send({ Error: "Error getting products" });
     }
+  }
 
-    async getProductById(req,res){
-        try {
-            const product = await productModel.findById(req.params.id);
-            if(!product){
-                throw new Error({'notFound':'product Not Found'})
-            }
-            const data = await productModel.find({'product_id': product.product_id});
-            res.send(data);
-        } catch (error) {
-            res.send({'Error':'Error getting product'});    
-        }
-     
+  async getProductById(req, res) {
+    try {
+      const product = await productModel.findById(req.params.id);
+      if (!product) {
+        throw new Error({ notFound: "product Not Found" });
+      }
+      const data = await productModel.find({ product_id: product.product_id });
+      res.send(data);
+    } catch (error) {
+      res.send({ Error: "Error getting product" });
     }
+  }
 
-
-    async getSegmentById(req,res){
-        try {
-            const segment = await productModel.findById(req.params.id);
-            if(!segment){
-                throw new Error({'notFound':'segment Not Found'})
-            }
-            res.send(segment);
-        } catch (error) {
-            res.send({'Error':'Error getting segment'});    
-        }
-     
+  async getSegmentById(req, res) {
+    try {
+      const segment = await productModel.findById(req.params.id);
+      if (!segment) {
+        throw new Error({ notFound: "segment Not Found" });
+      }
+      res.send(segment);
+    } catch (error) {
+      res.send({ Error: "Error getting segment" });
     }
+  }
 
+  async addProduct(req, res) {
+    try {
+      const product = await productModel();
+      for (let i = 0; i < req.files.length; i++) {
+        product.images.push(req.files[i].originalname);
+      }
+      (product.product_id = req.body.product_id),
+        (product.segment_id = req.body.segment_id),
+        (product.video_url = req.body.video_url),
+        (product.start_time = req.body.start_time),
+        (product.end_time = req.body.end_time),
+        (product.sequence_number = req.body.sequence_number),
+        (product.isPublished = req.body.isPublished),
+        (product.isArchive = req.body.isArchive),
+        await product.save();
+      res.send({ Successful: "product Added Successfully" });
+    } catch (e) {
+      res.send({ Error: "Error Added product" });
+    }
+  }
 
-    async addProduct(req, res){
-        try {
-            const product = await productModel();
-            for(let i=0;i<req.files.length;i++){
-                product.images.push(req.files[i].originalname)  
-            }
-            product.product_id = req.body.product_id,
-            product.segment_id = req.body.segment_id,
-            product.video_url = req.body.video_url,
-            product.start_time = req.body.start_time,
-            product.end_time = req.body.end_time,
-            product.sequence_number = req.body.sequence_number,
-            product.isPublished = req.body.isPublished, 
-            product.isArchive = req.body.isArchive,
-            await product.save();
-            res.send({'Successful':'product Added Successfully'})    
-        } catch (e) {
-            res.send({'Error':'Error Added product'}); 
-        }
-        
+  async updateProduct(req, res) {
+    const productId = req.params.id;
+    try {
+      if (!productId) {
+        throw new Error({ notFound: "productId Not Found" });
+      }
+      await productModel.update({ _id: productId }, { $set: req.body });
+      res.send({ Successful: "Update Successfully" });
+    } catch (e) {
+      res.send({ Error: "Error getting product update" });
     }
+  }
 
-    async updateProduct(req, res){
-        const productId = req.params.id;
-        try{
-            if(!productId){
-                throw new Error({'notFound':'productId Not Found'})
-            }
-            await productModel.update({_id:productId}, {$set:req.body});
-            res.send({'Successful':"Update Successfully"});
-        }catch (e){
-            res.send({'Error':'Error getting product update'});
-        }        
+  async deleteProduct(req, res) {
+    const productId = req.params.id;
+    try {
+      if (!productId) {
+        res.send({ notFound: "productId Not Found" });
+      }
+      const product = await productModel.deleteOne({ _id: productId });
+      res.send(product);
+    } catch (e) {
+      res.send({ Error: "Error getting product delete" });
     }
-    
-    async deleteProduct(req, res){
-        const productId = req.params.id;
-        try{
-            if(!productId){
-                res.send({'notFound':'productId Not Found'});
-            }
-            const product = await productModel.deleteOne({_id:productId});
-            res.send(product);
-        }catch (e){
-            res.send({'Error':'Error getting product delete'});
-        }    
-    }
+  }
 }
 module.exports = new Product();
